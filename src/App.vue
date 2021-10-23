29610b0802a13649fe7a703fb21b515f57ff0ba4
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-10-23 20:27:44 +0530

    add components

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 1,
+          title: 'Official Guide',
+          description: 'The official vue js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 2,
+          title: 'Google',
+          description:
+            'it is super important to learn google priror to learn anything',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
